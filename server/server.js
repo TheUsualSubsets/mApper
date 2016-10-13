89b9b3b1dc9585f502ed652da4f794d54bb135b6
Lizzie Shipton <lizzieshipton@Isabelles-MacBook.local>
2016-10-13 11:15:23 -0700

    connected to DB

--- server/server.js
+++ server/server.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var request = require('request');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', function(error){
+  console.error.bind(console, 'connection error: ')
+});
+db.once('open', function(){
+  console.log('connected to MONGODB');
+})
 
 var app = express();
-
 var PORT = process.env.PORT || 9001;
 
 app.use(express.static(__dirname + '/client'));
 
 var Map;
-
 request('https://maps.googleapis.com/maps/api/js?key=AIzaSyB0HuOvHVC8Rs8ZLtFoYfkoG2OJpxaZR70', function(err, res, body){
   if(err){
     console.error('Oops: ', error);
