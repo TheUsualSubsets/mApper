b2f4e8e3f6a5be69a5cbc2115ec7318ebe653a1a
Jeff Judkins <judkinsj@gmail.com>
2016-10-13 20:57:55 -0700

    correctly initializes mongoose database with location values

---
+++ server/databaseinitialization.js
@@ -0,0 +1,93 @@
+var mongoose = require('mongoose');
+var fs = require('fs');
+mongoose.connect('mongodb://localhost/loco');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+/*
+THIS FILE POPULATES/INITIALIZES THE MONGODB DATABASE FROM A CSV FILE THAT LISTS THE ID, CITY NAME, LATITUDE, LONGITUDE AND POINT OF INTEREST NAME FOR EACH POINT OF INTEREST.  ONCE THE DATABASE IS DEPLOYED, THIS WILL NOT NEED TO BE RUN ANYMORE.  ALL SUBSEQUENT ADDITIONS TO THE DATABASE WILL OCCUR VIA A SERVER REQEUST
+*/
+
+
+
+//CREATE A MONGOOSE SCHEMA FOR THE ENTRYSCHEMA DOCUMENT
+var entrySchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+
+  city_name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  lat: {
+    type: Number,
+    required: true
+  },
+  lng: {
+    type: Number,
+    required: true
+  },
+  poi: {
+    type: String,
+    required: true
+  }
+});
+
+
+//ASSIGNE THE MODEL CONSTRUCTOR TO A VARIABLE
+var entry = mongoose.model('entrySchema', entrySchema);
+
+
+
+//USE FS READFILE TO READ THE CSV FILE INTO MEMORY
+//NOTE ***IF CSV FILE IS LARGE, THE FOLLOWING MAY NEED TO BE REFACTORED
+//TO INCLUDE STREAMING/PIPING SO AS TO NOT TAKE UP SO MUCH VOLOTILE MEMORY 
+fs.readFile('LocationPoints.csv', 'utf-8', (err, data) => {
+  if (err) throw err;
+  //SPLIT EACH ENTRY INTO A SEPARATE STRING
+  var array = data.split('\r\n');
+  //REMOVE THE TITLE ROW
+  array.shift();
+  //INITIALIZE THE FINAL ARRAY
+  var finalArray = [];
+  //TRANFORM EACH ENTRY INTO AN OBJECT THAT CAN BE ADDED TO MONGODB
+  array.forEach(function(entry) {
+    //SPLIT EACH ENTRY INTO INDIVIDUAL ENTRY PROPERTIES
+    var entryArray = entry.split(',');
+    //CREATE AN OBJECT FOR EACH ENTRY - CHANGING STRINGS TO NUMBER FOR NUMBER 
+    //PROPERTIES
+    var entryObject = {
+      id: Number(entryArray[0]),
+      city_name: entryArray[1],
+      lat: Number(entryArray[2]),
+      lng: Number(entryArray[3]),
+      poi: entryArray[4]
+    }
+    //PUSH THE MODIFIED OBJECT TO THE ARRAY
+    finalArray.push(entryObject);
+  });
+  //USE ENTRY.CREATE TO CREATE NEW INSTANCES OF AN ENTRY AND TO SAVE THEM TO THE DATABASE
+  entry.create(finalArray, function(err, results) {
+    if(err) throw err;
+    console.log('success');
+  })
+});
+
+
+
+
+
+
+
+
+
+
+// city.create(chicago, function (err, chicago) {
+//   if (err) return handleError(err);
+//   console.log(chicago);
+// });
+
