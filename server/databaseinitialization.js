e5d7a18221e2ee3d2fa1f4361339c0be5189b157
Sean Enright <seanenright@Seans-MacBook-Pro.local>
2016-10-18 10:11:46 -0700

    fix all

--- server/databaseinitialization.js
+++ server/databaseinitialization.js
@@ -12,9 +12,7 @@ mongoose.connect('mongodb://104.236.129.131/mapper', function(err, db) {
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-/*
-THIS FILE POPULATES/INITIALIZES THE MONGODB DATABASE FROM A CSV FILE THAT LISTS THE ID, CITY NAME, LATITUDE, LONGITUDE AND POINT OF INTEREST NAME FOR EACH POINT OF INTEREST.  ONCE THE DATABASE IS DEPLOYED, THIS WILL NOT NEED TO BE RUN ANYMORE.  ALL SUBSEQUENT ADDITIONS TO THE DATABASE WILL OCCUR VIA A SERVER REQEUST
-*/
+
 
 
 
@@ -45,42 +43,26 @@ var entrySchema = new mongoose.Schema({
   }
 });
 
+//CREATE A MONGOOSE SCHEMA FOR THE CITY DOCUMENTS - THIS TABLE WILL JUST
+//STORE THE CITY NAMES WITH A UNIQUE ID
+var citylistSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  city_name: {
+    type: String, 
+    required: true,
+    unique: true
+  }
+});
+
 //ASSIGN THE MODEL CONSTRUCTOR TO A VARIABLE
 var entry = mongoose.model('entrySchema', entrySchema);
-module.exports = entry;
+var city = mongoose.model('citylistSchema', citylistSchema);
+module.exports.data = entry;
+module.exports.cityList = city;
 
 
-//USE FS READFILE TO READ THE CSV FILE INTO MEMORY
-// //NOTE ***IF CSV FILE IS LARGE, THE FOLLOWING MAY NEED TO BE REFACTORED
-// //TO INCLUDE STREAMING/PIPING SO AS TO NOT TAKE UP SO MUCH VOLITILE MEMORY
-fs.readFile('./locationPoints.csv', 'utf-8', (err, data) => {
-  if (err) throw err;
-  //SPLIT EACH ENTRY INTO A SEPARATE STRING
-  var array = data.split('\r\n');
-  //REMOVE THE TITLE ROW
-  array.shift();
-  //INITIALIZE THE FINAL ARRAY
-  var finalArray = [];
 
-  //TRANFORM EACH ENTRY INTO AN OBJECT THAT CAN BE ADDED TO MONGODB
-  array.forEach(function(entry) {
-    //SPLIT EACH ENTRY INTO INDIVIDUAL ENTRY PROPERTIES
-    var entryArray = entry.split(',');
-    //CREATE AN OBJECT FOR EACH ENTRY - CHANGING STRINGS TO NUMBER FOR NUMBER
-    //PROPERTIES
-    var entryObject = {
-      id: Number(entryArray[0]),
-      city_name: entryArray[1],
-      lat: Number(entryArray[2]),
-      lng: Number(entryArray[3]),
-      poi: entryArray[4]
-    }
-    //PUSH THE MODIFIED OBJECT TO THE ARRAY
-    finalArray.push(entryObject);
-  });
-  //USE ENTRY.CREATE TO CREATE NEW INSTANCES OF AN ENTRY AND TO SAVE THEM TO THE DATABASE
-  entry.create(finalArray, function(err, results) {
-    if(err) throw err;
-    console.log('success');
-  })
-});
--- server/dbqueries.js
+++ server/dbqueries.js
@@ -1,18 +1,146 @@
 var mongoose = require('mongoose');
 var random = require('mongoose-query-random');
-var City = require('./databaseinitialization.js');
+var db = require('./databaseinitialization.js');
 
 
 module.exports = {
+  //client expects response object in follwing form
+  //  {
+  //    position:     {lat: 40,
+  //                  lng: 40},
+  //    answer:       'Chicago',
+  //    poi:          'Navy Pier',
+  //    otherAnswers: ['London', 'Istanbul', 'San Francisco', 'New York City']
+  //  }
 
-  //randomly select a city from a list of names in DB
-  randomQuery: function(req, res){
-    var names = ['Chicago', 'San Francisco', 'London', 'Istanbul', 'New York'];
-      var randomIndex = Math.floor(Math.random() * names.length);
-      //use that city name to query database and return tuple of lat/long coords.
-      City.find().where('city_name').equals(names[randomIndex]).random(1, true, function (err, cities) {
-        //send coordinates back to client via server
-        res.send([cities[0].lat, cities[0].lng]);
-      });
-  }
+  //randomly select a city and poi from db
+  randomQuery: function(callback){
+    //call distinct query to get a list of unique cities from database
+    module.exports.distinctQuery(function(results) {
+      //shuffle the list of cities, returning only 5 in random order
+      //random order ensures answer list on client is random order
+      var cities = module.exports.shuffleArray(results, 5);
+      //select one city from the list to grab a point of database from database
+      var randomIndex = Math.floor(Math.random() * cities.length);
+      findRandomPOI(cities, cities[randomIndex]);
+    });
+
+    //use selected city name to query database and return tuple of lat/long 
+    //coords.
+    var findRandomPOI = function(cities, city) {
+      db.data.find().where('city_name').equals(city).then(function(results) {
+      //randomly select a point of interest from results
+      var randomIndex2 = Math.floor(Math.random() * results.length);
+      //assign lat and lng values
+      var lat = results[randomIndex2].lat;
+      var lng = results[randomIndex2].lng;
+      //create object to send back to client
+      var responseObject = 
+      {
+        position: {lat: lat, lng: lng},
+        answer: city,
+        poi: results[randomIndex2].poi,
+        answerChoices: cities
+      };
+      //send results back to client
+      callback(responseObject);
+    })
+      .catch(function(err) {
+        if(err) {
+          //send error back to client
+          callback(err)
+        }
+      })
+    }
+  },
+
+
+    //getAllQuery is mainly used for debugging purposes via postman
+    //it is a more convenient alternative to logging on to droplet and querying
+    //mongodB from there
+    getAllQuery: function(city, callback) {
+      //if a city was given as a query parameter, return only that cities' results
+      if(city) {
+        City.find().where('city_name').equals(city).then(function(results) {
+          callback(results);
+        })
+        .catch(function(err) {
+          callback(err);
+        }); 
+      } else {//if no city was given as a query parameter, return all results
+        City.find().then(function(results) {
+          callback(results);
+        })
+        .catch(function(err) {
+          callback(err);
+        })
+      }
+    },
+
+    //the following function is used for database maintenence.  If a value is 
+    //noticed to be incorrect, the databse can be updated by a simple post 
+    //request from postman.
+    updateEntry: function(lookup, update, callback) {
+      db.data.findOneAndUpdate(lookup, update, {new: true}, function(err, result) {
+        if(err) {
+          callback(err);
+        } else {
+          callback(result);
+        }
+      })
+
+    },
+
+    //this function was used for testing purposes but can be used to retrieve a list of all available cities.
+    distinctQuery: function(callback) {
+      db.data.distinct('city_name').then(function(result) {
+        console.log(result, 'result');
+        callback(result);
+      })
+      .catch(function(err) {
+        if(err) {
+          console.log(err, 'err')
+          callback(err);
+        }
+      })
+    },
+    //this uses a modified form of the fisher-yates shuffle to shuffle an array
+    shuffleArray: function(array, numOfItems) {
+        var originalLength = array.length;
+        var m = array.length, t, i;
+
+        // While there remain elements to shuffle…
+        while (m > originalLength - numOfItems) {
+
+          // Pick a remaining element…
+          i = Math.floor(Math.random() * m--);
+
+          // And swap it with the current element.
+          t = array[m];
+          array[m] = array[i];
+          array[i] = t;
+        }
+        //return a array with a length of numOfItems
+        var results = array.slice(originalLength - numOfItems);
+        console.log('shuffleresults', results);
+        return results;
+    }
 };
+
+// module.exports = {
+
+//   //randomly select a city from a list of names in DB
+//   randomQuery: function(callback){
+//     var names = ['Chicago', 'San Francisco', 'London', 'Istanbul', 'New York'];
+//       var randomIndex = Math.floor(Math.random() * names.length);
+//       //use that city name to query database and return tuple of lat/long coords.
+//       City.find().where('city_name').equals(names[randomIndex]).random(1, true, function (err, cities) {
+//         if (err) {
+//           callback(err)
+//         } else {
+//         //send coordinates back to client via server
+//           callback([cities[0].lat, cities[0].lng]);  
+//         }
+//       });
+//   }
+// };
